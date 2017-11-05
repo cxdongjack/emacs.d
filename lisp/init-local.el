fbe4399ea695c8b6bb493dde6725b86c9d7c3531
chenxiaodong <chenxiaodong@xiaomi.com>
2017-11-05 17:10:45 +0800

    feat: 增加了本地配置

---
+++ lisp/init-local.el
@@ -0,0 +1,48 @@
+;; 自定义配置
+
+;; 输入中文时不闪烁
+(setq redisplay-dont-pause nil)
+
+;;(set-frame-font "Monaco:pixelsize=12")
+;;(dolist (charset '(han kana symbol cjk-misc bopomofo))
+;;(set-fontset-font (frame-parameter nil 'font)
+;;                   charset
+;;                    (font-spec :family "Hiragino Sans GB" :size 14)
+;;))
+
+;; -----------------------------------------------------------------------------
+;; setting font for mac system
+;; -----------------------------------------------------------------------------
+;; Setting English Font
+(set-face-attribute
+ 'default nil :font "Monaco 14")
+;; Chinese Font 配制中文字体
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                    (font-spec :family "PingFang SC" :size 14)))
+
+(setq org-directory "~/git/org")
+(setq org-default-notes-file "~/git/org/refile.org")
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/git/org/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/git/org/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/git/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/git/org/diary.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/git/org/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/git/org/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file "~/git/org/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/git/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+
+(provide 'init-local)
